90dc6bf5e976593c3b50b275a3895dccae638bed
ryansuarez <ryan.t.suarez@vanderbilt.edu>
2025-01-28 17:21:08 -0600

    implemented piece mechanics

---
+++ backend/piece.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Piece:
+
+    def __init__(self, shape):
+        self.shape = np.array(shape)
+
+    def rotate(self):
+        self.shape = np.rot90(self.shape)
+
+    def flip(self):
+        self.shape = np.fliplr(self.shape)
+
+    def __rpr__(self):
+        return str(self.shape)
